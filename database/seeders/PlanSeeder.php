91d09904e05b4fa53b9b9d6c2d827990cf229991
unknown <ninedragonlabs@gmail.com>
2022-04-13 13:59:23 +0700

    update hurung P

--- database/seeders/PlanSeeder.php
+++ database/seeders/PlanSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\plan;
+use App\Models\Plan;
 
 class PlanSeeder extends Seeder
 {
